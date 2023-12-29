d2556b174ce8de9886c94c7e335a3b63d25cb9db
bonnce <bonce_reloko@hotmail.com>
2023-12-29 17:38:51 -0300

    button, card and circle components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'ten':'#F2B600',
+				'thirty':'#404C80',
+				'sixty':'#00030F',
+			},
+			dropShadow:{
+				'text':'0px 2px 2px #0005'
+			}
+		},
 	},
 	plugins: [],
 }
